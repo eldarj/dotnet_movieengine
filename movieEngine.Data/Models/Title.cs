c5bd441407cbde129d5b801973254a606b9edb58
eldarj <eldar.jah@gmail.com>
2019-04-16 19:32:51 +0200

    Removing migrations and Scaffolding generated earlier while using SQL Server.

--- movieEngine.Data/Models/Title.cs
+++ movieEngine.Data/Models/Title.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 
 namespace movieEngine.Data.Models
 {
@@ -18,6 +19,11 @@ namespace movieEngine.Data.Models
 
         public TitleType Type { get; set; }
 
+        /**
+         * JsonIgnore - for the purpose of this test-project, just ignore some entity 
+         * model fields during serialization (otherwise the API should always map and return response/view models and not entities)
+         */
+        [JsonIgnore]
         public virtual ICollection<TitleActor> Actors { get; set; }
     }
 }
