d7eb1d4393d8196533b38f2efbe8feb0d0ef9b60
eldarj <eldar.jah@gmail.com>
2019-04-20 10:21:13 +0200

    Adding type api endpoint - for fetching all types of titles.

---
+++ movieEngine/Areas/Api/Controllers/TypeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using movieEngine.Data;
+using movieEngine.Web.Areas.Api.Helpers;
+
+namespace movieEngine.Web.Areas.Api.Controllers
+{
+    [Route("api/types")]
+    public class TypeController : MyBaseApiController
+    {
+        public TypeController(MyDbContext ctx, IMapper mapper) : base(ctx, mapper) { }
+
+        // GET: api/types
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var types = db.TitleTypes.ToList();
+            return Ok(types);
+        }
+    }
+}
